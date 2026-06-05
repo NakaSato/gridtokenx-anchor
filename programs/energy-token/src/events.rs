6db0cb1bd75156168ef461e37536a3b6b3adfb6e
WiT <wit.chanthawat@gmail.com>
2026-06-05 22:00:07 +0700

    Update events.rs

--- programs/energy-token/src/events.rs
+++ programs/energy-token/src/events.rs
@@ -10,31 +10,9 @@ pub struct GridTokensMinted {
 }
 
 #[event]
-pub struct TokensMintedDirect {
-    pub recipient: Pubkey,
-    pub amount: u64,
-    pub timestamp: i64,
-}
-
-#[event]
 pub struct TokensMinted {
     pub recipient: Pubkey,
     pub amount: u64,
-    pub timestamp: i64,
-}
-#[event]
-pub struct MeterReadingRecorded {
-    pub meter_owner: Pubkey,
-    pub meter_serial: String,
-    pub energy_kwh: u64,
-    pub timestamp: i64,
-}
-
-#[event]
-pub struct EnergyConsumed {
-    pub meter_owner: Pubkey,
-    pub energy_consumed_kwh: u64,
-    pub tokens_burned: u64,
     pub timestamp: i64,
 }
 
